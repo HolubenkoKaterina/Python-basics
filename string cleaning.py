5e05df942116fc9eb5a6fb9824624d4c11b324d8
Катерина Голубенко <109754382+HolubenkoKaterina@users.noreply.github.com>
2023-11-24 12:51:32 +0200

    Add files via upload

---
+++ string cleaning.py
@@ -0,0 +1,5 @@
+# text = 'i like python! i can learn it! yes? it is difficult? but i can, i believe.'
+# import string
+# translator = str.maketrans('', '', string.punctuation)
+# new_text = text.translate(translator)
+# print(new_text)
